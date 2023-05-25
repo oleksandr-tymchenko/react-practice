3650232973ba584c1ff978a0fa78a6f4ed3520b5
Oleksandr Tymchenko <116674828+oleksandr-tymchenko@users.noreply.github.com>
2023-05-25 14:07:07 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-
-import {Card} from './compponents/Card'
+// import {Card} from './compponents/Card'
 
 // {
-    
+
 //     "id": 1,
 //     "title": "accusamus beatae ad facilis cum similique qui sunt",
 //     "url": "https://via.placeholder.com/600/92c952",
 //     "thumbnailUrl": "https://via.placeholder.com/150/92c952"
 //   },
-
 
 // const paragraph = React.createElement('p', p,})
 // const p = <p id = 'test-p-id' >I am p</p>
@@ -23,8 +21,6 @@ import {Card} from './compponents/Card'
 // console.log(div)
 
 // сюда приходить об'єкт параметрів які передані при виклику <Head clas='card-title' id='id-123'/>
-
-  
 
 // const Сard = () => {
 //   return data.map((photo) => {
@@ -51,18 +47,87 @@ import {Card} from './compponents/Card'
 
 //   })
 // }
-  
+
 // console.log('card', )
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(<Card/>)
+// ReactDOM.createRoot(document.getElementById('root')).render(<Card/>)
 // import ReactDOM from 'react-dom/client';
 // import './index.css';
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
-//   <React.StrictMode> 
+//   <React.StrictMode>
 //   </React.StrictMode>
 // );
 
+// Виносимо об'єкт із примітивами в константу, щоб було зручно скидати.
+// Не можна використовувати, якщо в якійсь властивості стану зберігається складний тип.
+const INITIAL_STATE = {
+  login: "",
+  email: "",
+  password: "",
+};
 
+class SignUpForm extends React.Component {
+  state = { ...INITIAL_STATE };
+
+  // Для всіх інпутів створюємо один обробник
+  // Розрізняти інпути будемо за атрибутом name
+  handleChange = (evt) => {
+    const { name, value } = evt.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = (evt) => {
+    evt.preventDefault();
+    const { login, email, password } = this.state;
+    console.log(`Login: ${login}, Email: ${email}, Password: ${password}`);
+    this.props.onSubmit({ ...this.state });
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ ...INITIAL_STATE });
+  };
+
+  render() {
+    const { login, email, password } = this.state;
+
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name
+          <input
+            type="text"
+            placeholder="Enter login"
+            name="login"
+            value={login}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label>
+          Email
+          <input
+            type="email"
+            placeholder="Enter email"
+            name="email"
+            value={email}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label>
+          Password
+          <input
+            type="password"
+            placeholder="Enter password"
+            name="password"
+            value={password}
+            onChange={this.handleChange}
+          />
+        </label>
+
+        <button type="submit">Sign up as {login}</button>
+      </form>
+    );
+  }
+}
